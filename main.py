3f3da7c386a8329e3d3dc19b46364c3934652fc0
isaac-18 <isaac.curiel6721@gmail.com>
2020-09-22 22:21:39 -0700

    Rough prototype done
    *Append new parking data to existing file every five mins

--- main.py
+++ main.py
@@ -1,38 +1,64 @@
 from requests_html import HTMLSession
+import openpyxl
+import time
 
-session = HTMLSession()
 
-url = 'https://parkingapps.ucr.edu/spaces/'
+def getParkingData():
+    session = HTMLSession()
 
-r = session.get(url)
+    url = 'https://parkingapps.ucr.edu/spaces/'
 
-r.html.render(sleep=1, keep_page=True, scrolldown=1)
+    r = session.get(url)
 
-lots = r.html.find('.col-sm-6')
+    r.html.render(sleep=1, keep_page=True, scrolldown=1)
 
-filename = 'parking_data.csv'
-f = open(filename, 'w')
+    lots = r.html.find('.col-sm-6')
+    parking_lots = []
 
-headers = 'lot, empty_spaces, time\n'
-f.write(headers)
+    for i in range(0, len(lots) - 1):
+        # Info for each lot is returned as list of strings.
+        # All info for single lot is a single string with data seperated with newlines
+        # e.g ('Lot 24-\n3:40pm\nCanyon Crest Drive\nFree Spaces\n405\nOccupancy\n0%\n')
+        split_string = lots[i].text.split('\n') 
+        parking_lot = {
+            'Parking Lot' : split_string[0].replace('-', ''),
+            'Time' : split_string[1],
+            'Free Spaces' : split_string[4],
+            'Occupancy' : split_string[6],
+        } 
+        print(parking_lot)
 
-for i in range(0, len(lots) - 1):
-    # Info for each lot is returned as list of strings.
-    # All info for single lot is a single string with data seperated with newlines
-    # e.g ('Lot 24-\n3:40pm\nCanyon Crest Drive\nFree Spaces\n405\nOccupancy\n0%\n')
-    split_string = lots[i].text.split('\n') 
-    parking_lot = {
-        'Parking Lot' : split_string[0].replace('-', ''),
-        'Time' : split_string[1],
-        'Free Spaces' : split_string[4],
-        'Occupancy' : split_string[6],
-    } 
-    print(parking_lot)
+        parking_lots.append(parking_lot)
+    
+    session.close()
 
-    f.write(parking_lot.get('Parking Lot') + ',' + parking_lot.get('Free Spaces') + ',' + parking_lot.get('Time') + '\n')
+    return parking_lots
 
-f.close()
-session.close()
+
+filename = 'parking_data.xlsx'
+wb = openpyxl.load_workbook(filename)
+# ws1 = wb['big_springs']
+allSheetNames = wb.sheetnames
+
+
+for row in range(2, 10):
+    lotCnt = 0
+    time_cell = "{}{}".format('A', row)
+    spaces_cell = "{}{}".format('F', row)
+
+    parking_data = getParkingData()
+
+    for sheet in allSheetNames:
+        currentSheet = wb[sheet]
+        currentSheet[time_cell] = parking_data[lotCnt].get('Time')
+        currentSheet[spaces_cell] = parking_data[lotCnt].get('Free Spaces')
+        lotCnt += 1
+
+    wb.save(filename)
+    time.sleep(300)
+ 
+
+
 
 # # ---- Gets and displays all info for all parking lots (for testing) ----
 # lots = r.html.find('.col-sm-6')
